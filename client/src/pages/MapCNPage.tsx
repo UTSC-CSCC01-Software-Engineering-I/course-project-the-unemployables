f0823152f00d7d131bc89c053180e9974782b3a2
Tri Nguyen <tous0905@gmail.com>
2026-06-21 22:24:51 -0400

    Added popup UI for invalid filters

--- client/src/pages/MapCNPage.tsx
+++ client/src/pages/MapCNPage.tsx
@@ -1,13 +1,18 @@
 import { Map, MapControls } from "@/components/ui/map";
+import { InvalidFilterPopUp } from "@/components/ui/invalidFilterPopUp";
 import "./MapCNPage.css";
 
 export function MapCNPage() {
   return (
     <div style={{ flex: 1, minHeight: 0 }}>
+      <InvalidFilterPopUp />
       <Map
         center={[-96, 62]}
         zoom={3.2}
-        maxBounds={[[-145, 40], [-45, 86]]}
+        maxBounds={[
+          [-145, 40],
+          [-45, 86],
+        ]}
         minZoom={2.5}
         className="h-full w-full"
       >
@@ -16,4 +21,3 @@ export function MapCNPage() {
     </div>
   );
 }
-
