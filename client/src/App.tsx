49b9f776334aaeaca4b803149e3bbefdd67de292
Akkipr <akshayanpraba6@gmail.com>
2026-07-06 11:50:38 -0400

    feat: updated App.tsx to include new routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,16 +4,19 @@ import { Sidebar } from "./components/layout/Sidebar";
 import { HomePage } from "./pages/HomePage";
 import { MapCNPage } from "./pages/MapCNPage";
 import { ResearcherLoginUIpage } from "./pages/ResearcherLogin";
+import { ResearcherDashboardPage } from "./pages/ResearcherDashboardPage";
 import "./App.css";
 
 function AppLayout() {
   const location = useLocation();
   const hideSidebarRoutes = ['/login', '/map'];
+  const hideTopNavRoutes = ['/login'];
   const showSidebar = !hideSidebarRoutes.includes(location.pathname);
+  const showTopNav = !hideTopNavRoutes.includes(location.pathname);
 
   return (
       <div className="app">
-        <TopNav />
+        {showTopNav && <TopNav />}
         <div className="app-body">
           {showSidebar && <Sidebar />}
           <main className="app-main">
@@ -21,6 +24,7 @@ function AppLayout() {
               <Route path="/" element={<HomePage />} />
               <Route path="/map" element={<MapCNPage />} />
               <Route path="/login" element={<ResearcherLoginUIpage />} />
+              <Route path="/advanced-filters" element={<ResearcherDashboardPage />} />
             </Routes>
           </main>
         </div>
